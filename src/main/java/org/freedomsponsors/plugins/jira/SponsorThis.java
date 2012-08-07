1f6d0dd079615a2b11ab9c4baeb7b25f1ae09558
Arthur Casals <arthur.casals@freedomsponsors.org>
2012-08-07 06:10:29 -0300

    Corrected context bugs at atlassian-plugin.xml

--- src/main/java/org/freedomsponsors/plugins/jira/SponsorThis.java
+++ src/main/java/org/freedomsponsors/plugins/jira/SponsorThis.java
@@ -2,6 +2,7 @@ package org.freedomsponsors.plugins.jira;
 
 import com.atlassian.crowd.embedded.api.User;
 import com.atlassian.jira.config.properties.APKeys.JiraIndexConfiguration;
+import com.atlassian.jira.config.properties.ApplicationProperties;
 import com.atlassian.jira.functest.config.JiraConfig;
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.plugin.webfragment.contextproviders.AbstractJiraContextProvider;
@@ -15,15 +16,21 @@ import java.util.Map;
 
 public class SponsorThis extends AbstractJiraContextProvider
 {       
+	private final ApplicationProperties applicationProperties;
+
+	public SponsorThis(ApplicationProperties applicationProperties) // magic constructor
+    {
+        this.applicationProperties = applicationProperties;
+    }
 	
 	@Override
     public Map getContextMap(User user, JiraHelper jiraHelper) {
         Map contextMap = new HashMap();        
-        //String baseUrl = (String)jiraHelper.getContextParams().get("baseurl");
+        String baseUrl = applicationProperties.getString("jira.baseurl");
         Issue currentIssue = (Issue) jiraHelper.getContextParams().get("issue");        
         String issueKey = currentIssue.getKey();        
         contextMap.put("issueKey", issueKey);
-        //contextMap.put("baseUrl", baseUrl);
+        contextMap.put("baseUrl", baseUrl);
         
         
         return contextMap;
