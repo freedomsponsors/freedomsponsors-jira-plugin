eaf4eb5f48203af5aefc607c9463a5fb6846b233
tony.franca <tony.franca@touchtec.com.br>
2012-08-07 15:33:01 -0300

    Small adjustments

--- src/main/java/org/freedomsponsors/plugins/jira/SponsorThis.java
+++ src/main/java/org/freedomsponsors/plugins/jira/SponsorThis.java
@@ -1,18 +1,13 @@
 package org.freedomsponsors.plugins.jira;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.atlassian.crowd.embedded.api.User;
-import com.atlassian.jira.config.properties.APKeys.JiraIndexConfiguration;
 import com.atlassian.jira.config.properties.ApplicationProperties;
-import com.atlassian.jira.functest.config.JiraConfig;
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.plugin.webfragment.contextproviders.AbstractJiraContextProvider;
 import com.atlassian.jira.plugin.webfragment.model.JiraHelper;
-import com.atlassian.jira.util.http.JiraUrl;
-
- 
-import java.sql.Timestamp;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SponsorThis extends AbstractJiraContextProvider
 {       
